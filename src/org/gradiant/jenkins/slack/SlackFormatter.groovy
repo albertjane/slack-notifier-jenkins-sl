1cbeef9804e450a0293663b637a08e7bfa1ca928
Albert Jane <albertjane@users.noreply.github.com>
2021-02-22 17:50:57 +0100

    Adding Changes link

--- src/org/gradiant/jenkins/slack/SlackFormatter.groovy
+++ src/org/gradiant/jenkins/slack/SlackFormatter.groovy
@@ -8,14 +8,15 @@ String format(String title = '', String message = '', String testSummary = '') {
     def branchName = helper.getBranchName()
     def buildNumber = helper.getBuildNumber()
     def url = helper.getAbsoluteUrl()
-    def urlBlue = helper.getBlueUrl()
+    def urlBlueResult = helper.getBlueResultUrl()
+    def urlBlueChanges = helper.getBlueChangesUrl()
 
     def result = "${project}"
 
     if (branchName != null) result = "${result} » ${branchName}"
 
-    result = "${result} - #${buildNumber} ${title.trim()} (<${url}|Open>) (<${urlBlue}|BlueOcen>)"
-    if (message) result = result + "\nChanges:\n\t ${message.trim()}"
+    result = "${result} - #${buildNumber} ${title.trim()} (<${urlBlue}|Build Results>) - (<${url}|Classic>)"
+    if (message) result = result + "\n<${urlBlueChanges}|Changes>:\n\t ${message.trim()}"
     if (testSummary) result = result + "\n ${testSummary}"
 
     return result
