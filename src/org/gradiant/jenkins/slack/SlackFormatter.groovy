f43fe1ed2f9d278d907db45c6dadb4f62115171c
Albert Jane <albertjane@users.noreply.github.com>
2021-02-22 14:31:52 +0100

    Adding Blue Ocean URL

--- src/org/gradiant/jenkins/slack/SlackFormatter.groovy
+++ src/org/gradiant/jenkins/slack/SlackFormatter.groovy
@@ -8,12 +8,13 @@ String format(String title = '', String message = '', String testSummary = '') {
     def branchName = helper.getBranchName()
     def buildNumber = helper.getBuildNumber()
     def url = helper.getAbsoluteUrl()
+    def urlBlue = helper.getBlueUrl()
 
     def result = "${project}"
 
     if (branchName != null) result = "${result} » ${branchName}"
 
-    result = "${result} - #${buildNumber} ${title.trim()} (<${url}|Open>)"
+    result = "${result} - #${buildNumber} ${title.trim()} (<${url}|Open>) (<${urlBlue}|BlueOcen>)"
     if (message) result = result + "\nChanges:\n\t ${message.trim()}"
     if (testSummary) result = result + "\n ${testSummary}"
 
